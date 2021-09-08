f7f0ad70b5e4017bbfdba02ed0c8718baab26551
Jared Silver <jrsilver88@gmail.com>
2021-09-07 21:33:19 -0700

    created the brick tiles

--- script.js
+++ script.js
@@ -29,6 +29,27 @@ const paddle = {
   dx: 0,
 };
 
+// Create brick props
+const brickInfo = {
+  w: 70,
+  h: 20,
+  padding: 10,
+  offsetX: 45,
+  offsetY: 60,
+  visible: true,
+};
+
+// Create bricks
+const bricks = [];
+for (let i = 0; i < brickRowCount; i++) {
+  bricks[i] = [];
+  for (let j = 0; j < brickColumnCount; j++) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+    bricks[i][j] = { x, y, ...brickInfo };
+  }
+}
+
 // Draw ball on canvas
 function drawBall() {
   ctx.beginPath();
@@ -47,17 +68,30 @@ function drawPaddle() {
   ctx.closePath();
 }
 
+// Draw score on canvas
+function drawScore() {
+  ctx.font = '20px Arial';
+  ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+}
+
+function drawBricks() {
+  bricks.forEach((column) => {
+    column.forEach((brick) => {
+      ctx.beginPath();
+      ctx.rect(brick.x, brick.y, brick.w, brick.h);
+      ctx.fillStyle = brick.visible ? '#0095dd' : 'transparent';
+      ctx.fill();
+      ctx.closePath();
+    });
+  });
+}
+
 // Draw everything
 function draw() {
   drawBall();
   drawPaddle();
   drawScore();
-}
-
-// Draw score on canvas
-function drawScore() {
-  ctx.font = '20px Arial';
-  ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+  drawBricks();
 }
 
 draw();
